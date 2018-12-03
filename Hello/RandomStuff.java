8413f52173a0fcf2b53a1d366f80d5d1fd94393f
Paimon Goulart <paimon.gou@lvjusd.k12.ca.us>
2018-12-03 12:57:26 -0800

    added another class random stuff

---
+++ Hello/RandomStuff.java
@@ -0,0 +1,12 @@
+
+public class RandomStuff
+{
+  public static void main(){
+      for(int i = 0; i<5; i = i +1){
+      int age = (int)(Math.random()*8+367);
+      System.out.println("the number is " + age );
+      
+    }
+      
+    }
+}

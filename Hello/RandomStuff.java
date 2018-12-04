5b41674388d9e4464645e0577097da99826c7f90
Paimon Goulart <paimon.gou@lvjusd.k12.ca.us>
2018-12-04 12:06:30 -0800

    made some changes

--- Hello/RandomStuff.java
+++ Hello/RandomStuff.java
@@ -5,7 +5,7 @@ public class RandomStuff
       for(int i = 0; i<5; i = i +1){
       int age = (int)(Math.random()*8+367);
       System.out.println("the number is " + age );
-      System.out.println("hello");
+      System.out.println("hello it's tuesday");
       
     }
       
---
+++ Hello1/hey.java
@@ -0,0 +1,7 @@
+
+public class hey
+{
+    public static void main(){
+        System.out.println("hello");
+    }
+}
